cbdbb81ce950933f62b3f5ffb0cb2ba65f0c7ae3
VictoriaKennet13 <52086449+VictoriaKennet13@users.noreply.github.com>
2020-02-23 00:49:07 +0200

    test

--- src/main/java/com/aplication/weather/controller/MainController.java
+++ src/main/java/com/aplication/weather/controller/MainController.java
@@ -1,25 +1,45 @@
 package com.aplication.weather.controller;
 
 import com.aplication.weather.model.Weather;
-import org.springframework.ui.Model;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class MainController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/todayWeather")
+    public Weather todayWeather() throws IOException {
+        Weather weather = new Weather();
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://community-open-weather-map.p.rapidapi.com/weather?lat=34.80029&lon=50.9216&callback=sumy&id=2172797&lang=en&units=metric&q=Sumy%252CUkraine")
+                .get()
+                .addHeader("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com")
+                .addHeader("x-rapidapi-key", "b26509b3aamsh3d8a4988cb96eb8p1a8cefjsnd0c8f941c941")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println(response);
+
+        return weather;
     }
 
-    @GetMapping("/point1")
-    public Weather someInf() {
+    @GetMapping("/nextDayWeather")
+    public Weather nextDayWeather() {
         Weather weather = new Weather();
-        weather.setID(1);
-        weather.setName("TEST name");
+        return weather;
+    }
+
+    @GetMapping("/topWeather")
+    public Weather topWeather() {
+        Weather weather = new Weather();
         return weather;
     }
 }
--- src/main/java/com/aplication/weather/model/Weather.java
+++ src/main/java/com/aplication/weather/model/Weather.java
@@ -1,22 +1,70 @@
 package com.aplication.weather.model;
 
 public class Weather {
-    private int ID;
-    private String name;
+    private String ID;
+    private double temperature;
+    private String description;
+    private double windSpeed;
+    private String windDirection;
+    private double cloudCover;
 
-    public int getID() {
+    public Weather() {
+    }
+
+    public Weather(String ID, double temperature, String description, double windSpeed, String windDirection, double cloudCover) {
+        this.ID = ID;
+        this.temperature = temperature;
+        this.description = description;
+        this.windSpeed = windSpeed;
+        this.windDirection = windDirection;
+        this.cloudCover = cloudCover;
+    }
+
+    public String getID() {
         return ID;
     }
 
-    public void setID(int ID) {
+    public void setID(String ID) {
         this.ID = ID;
     }
 
-    public String getName() {
-        return name;
+    public double getTemperature() {
+        return temperature;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getWindDirection() {
+        return windDirection;
+    }
+
+    public void setWindDirection(String windDirection) {
+        this.windDirection = windDirection;
+    }
+
+    public double getCloudCover() {
+        return cloudCover;
+    }
+
+    public void setCloudCover(double cloudCover) {
+        this.cloudCover = cloudCover;
     }
 }
