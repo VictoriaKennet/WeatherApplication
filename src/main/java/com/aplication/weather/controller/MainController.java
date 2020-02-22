6ab130de45b3c8b3e9c2726631af8362f9fac554
VictoriaKennet13 <52086449+VictoriaKennet13@users.noreply.github.com>
2020-02-22 14:29:29 +0200

    add main controller

---
+++ src/main/java/com/aplication/weather/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.aplication.weather.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class MainController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        model.put("some","Hi!");
+        return "main";
+    }
+}
